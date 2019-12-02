6caf1e686fe36dbffb0e950eedac4ec48cf74c15
idnes.link <16096613+idnesdotlink@users.noreply.github.com>
2019-12-02 08:49:33 +0700

    init

---
+++ vue.config.js
@@ -0,0 +1,38 @@
+const webpack = require("webpack");
+// const isProd = process.env.NODE_ENV === "production";
+
+module.exports = {
+  configureWebpack: {
+    // Set up all the aliases we use in our app.
+    plugins: [
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 6
+      })
+    ]
+  },
+  pwa: {
+    name: "Vue Argon Dashboard",
+    themeColor: "#172b4d",
+    msTileColor: "#172b4d",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "#172b4d"
+  },
+  css: {
+    // Enable CSS source maps.
+    sourceMap: process.env.NODE_ENV !== "production",
+    loaderOptions: {
+      sass: {
+        implementation: require("sass"), // This line must in sass option
+        sassOptions: {
+          fiber: require("fibers")
+        }
+      },
+      scss: {
+        implementation: require("sass"), // This line must in sass option
+        sassOptions: {
+          fiber: require("fibers")
+        }
+      }
+    }
+  }
+};
